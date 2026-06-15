a50c5e860ecf1e665834805859d1c9a3c0188ffa
maxie-corse <maxie.corse.forza@gmail.com>
2026-06-16 01:14:16 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
+
+    static boolean isBuiltin(String command) {
+        return command.equals("exit") || command.equals("echo") || command.equals("type");
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-
-        String input, command, arguments;
 
         while (true) {
             System.out.print("$ ");
 
-            input = sc.nextLine();
+            String input = sc.nextLine();
             int space_index = input.indexOf(' ');
 
-            if (space_index == -1) {
-                command = input;
-                arguments = "";
-            }
-            else {
-                command = input.substring(0, space_index);
-                arguments = input.substring(space_index + 1);
-            }
+            String command = (space_index == -1)? input : input.substring(0, space_index);
+            String arguments = (space_index == -1)? "" : input.substring(space_index + 1);
 
             if (command.equals("exit")) {
                 break;
@@ -28,7 +25,7 @@ public class Main {
                 System.out.println(arguments);
             }
             else if (command.equals("type")) {
-                if (arguments.equals("exit") || arguments.equals("echo") || arguments.equals("type")) {
+                if (isBuiltin(arguments)) {
                     System.out.println(arguments + " is a shell builtin");
                 }
                 else {
