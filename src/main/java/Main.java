7b10f93c05d48eb8ea013240108e53ecf01ae31a
maxie-corse <maxie.corse.forza@gmail.com>
2026-06-16 01:24:50 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,31 @@
 import java.util.Scanner;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
 
     static boolean isBuiltin(String command) {
         return command.equals("exit") || command.equals("echo") || command.equals("type");
+    }
+
+    static String findExecutable(String command) {
+        String pathEnv = System.getenv("PATH");
+
+        if (pathEnv == null) return "";
+
+        String[] directories = pathEnv.split(File.pathSeparator);
+
+        for (String directory : directories) {
+            Path fullPath = Path.of(directory, command);
+
+            if (Files.exists(fullPath) && Files.isRegularFile(fullPath) && Files.isExecutable(fullPath)) {
+                return fullPath.toString();
+            }
+        }
+
+        return "";
     }
 
     public static void main(String[] args) throws Exception {
@@ -13,10 +35,10 @@ public class Main {
             System.out.print("$ ");
 
             String input = sc.nextLine();
-            int space_index = input.indexOf(' ');
+            int spaceIndex = input.indexOf(' ');
 
-            String command = (space_index == -1)? input : input.substring(0, space_index);
-            String arguments = (space_index == -1)? "" : input.substring(space_index + 1);
+            String command = (spaceIndex == -1)? input : input.substring(0, spaceIndex);
+            String arguments = (spaceIndex == -1)? "" : input.substring(spaceIndex + 1);
 
             if (command.equals("exit")) {
                 break;
@@ -29,7 +51,14 @@ public class Main {
                     System.out.println(arguments + " is a shell builtin");
                 }
                 else {
-                    System.out.println(arguments + ": not found");
+                    String executablePath = findExecutable(arguments);
+
+                    if (!executablePath.isEmpty()) {
+                        System.out.println(arguments + " is " + executablePath);
+                    }
+                    else {
+                        System.out.println(arguments + ": not found");
+                    }
                 }
             }
             else {
