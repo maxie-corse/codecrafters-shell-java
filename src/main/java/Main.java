a1c3626f7ada4f3b71945b4844263d0c776f6364
maxie-corse <maxie.corse.forza@gmail.com>
2026-06-16 00:46:47 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
         while (true) {
             System.out.print("$ ");
-            Scanner sc = new Scanner(System.in);
             String input = sc.nextLine();
             System.out.println(input + ": command not found");
         }
